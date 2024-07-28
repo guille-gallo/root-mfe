d353e88352d7651802fd28c18d330bd292bc9a46
guille-gallo <guille.gallo88@gmail.com>
2024-07-28 18:02:35 +0200

    Revert "Merge branch 'gh-pages' of https://github.com/guille-gallo/root-mfe into gh-pages"
    This reverts commit 787ae13b63991c355dd3a705cd7961e9053ac8ca, reversing
    changes made to 9a18dcddefc12ce64c006d093bbe21e43fe3f6a9.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "guille";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+    disableHtmlGeneration: true,
+  });
+
+  return merge(defaultConfig, {
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+    ],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+    },
+  });
+};
